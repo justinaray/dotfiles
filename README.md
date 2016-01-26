abf10ccde1dac746025f6cd4f1ae8ffbbd36b017
justinaray <justin.a.ray@gmail.com>
2016-01-26 00:43:02 -0500

    WIP ... Initial files

---
+++ README.md
@@ -0,0 +1,8 @@
+# My .files
+
+This repo isn't nearly as fancy as most.  It's mostly just a personal backup and very much a work in progress.  Take what you want, but _*Use at your own risk*_
+
+# TODO
+* brew/taps/cask/install
+* node/nvm
+* osx defaults
---
+++ zsh/general.zsh
@@ -0,0 +1,29 @@
+#Prompt
+export PS1="\w 🚀 > "
+
+#Colors
+#export CLICOLOR=1
+#export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
+
+#Global aliases (ZSH Specific)
+alias -g code='~/code'
+
+#Aliases
+#alias ll="ls -laF"
+alias year='cal 2016'
+alias npm-exec='PATH=$(npm bin):$PATH'
+alias code='cd ~/code'
+alias docs='cd ~/Documents'
+
+#fns
+function title {
+    echo -en "\e]1;$1\a"
+}
+
+function titleWin {
+    echo -en "\e]2;$1\a"
+}
+
+#NVM
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
