3a138c55fccbe34fce1eaab27bd6379ece7b7aba
justinaray <justin.a.ray@gmail.com>
2019-05-15 12:34:29 -0400

    Git: 	switch to use include-if for directory based default config and per-machine custom config via includes 	Add example .gitconfig-personal-oss 	Remove default user from .gitconfig as it has been replaced by includes 	Remove atom as the default editor and invalid excludesfiles value
    Zsh:
    	Add bash history control
    	Port in zsh specific config from ~/.zshrc so it’s more portable across machines
    		theme and auto title
    	Fix less arguments
    	Add directory aliases
    	Update nvm to latest setup style
    	Add groovy
    	Bump add-daily-key to 10 hours to exceed a typical dev day

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -1,32 +1,44 @@
 # Prompt
 export PS1="\w 🚀 > "
-export PROMPT=$PS1
+
+# Leading space does not store command in history
+export HISTCONTROL=ignorespace
+setopt HIST_IGNORE_SPACE
 
 # Editor
 export EDITOR=/usr/bin/vim
 export VISUAL=$EDITOR
 
-# Colors
+# Themes and Colors
+ZSH_THEME="gallois"
+
 #export CLICOLOR=1
 #export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
 
 # Common Programs
-export LESS=-i
 
-#Global aliases (ZSH Specific)
-alias -g code='~/code'
+## Less
+### quit-if-one-screen and no-init will perform like cat for short files
+export LESS='--ignore-case --quit-if-one-screen --no-init --hilite-unread -R'
 
 # Aliases
+
+## Util Aliases
 #alias ll="ls -laF"
 alias year="cal $(date +'%Y')"
-alias code='cd ~/code'
+
+## Common Directory Aliases
+alias mycode='cd ~/code'
 alias docs='cd ~/Documents'
+alias pics='~/Pictures'
+alias dls='cd ~/Downloads'
 
 # Application Aliases
 alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 alias chrome-canary="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"
 
 # fns
+DISABLE_AUTO_TITLE="true"
 function title {
     echo -en "\e]1;$1\a"
 }
@@ -40,8 +52,9 @@ alias npm-exec='PATH=$(npm bin):$PATH'
 alias nombom='npm cache clear && bower cache clean && rm -rf node_modules bower_components && npm install && bower install'
 
 # NVM
-export NVM_DIR=~/.nvm
-source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
 
 # Yarn
 #PATH="$PATH:`yarn global bin`"
@@ -56,6 +69,10 @@ export M2=$M2_HOME/bin
 #export MAVEN_OPTS=-Xms256m -Xmx1048m -XX:MaxPermSize=312M
 export PATH=$JAVA_HOME/bin:$M2_HOME/bin:$PATH
 
-# Add the specified key to the ssh agent with an 8 hr expiry
+# Groovy Stuff
+export GROOVY_HOME=/usr/local/opt/groovy/libexec
+
+# SSH
+# Add the specified key to the ssh agent with an 10 hr expiry
 # Don't forget to start the agent with eval "$(ssh-agent -s)"
-alias add-daily-key='ssh-add -t "$((60 * 60 * 8))"'
+alias add-daily-key='ssh-add -t "$((60 * 60 * 10))"'
