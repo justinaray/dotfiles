f19f567802917866b6932a30d91a77e2eba41d69
justinaray <justin.a.ray@gmail.com>
2025-07-19 12:04:20 -0400

    Update mac setup
    Finder - configure default view options
    Textedit - default mode is plain text
    Enable tab nav in system dialogs
    Configure globe key to emoji picker

--- zsh/macOs.zsh
+++ zsh/macOs.zsh
@@ -10,15 +10,39 @@ elif [ -L "$HOME/tmp" ] && [ -d "$HOME/tmp" ]; then
   fi
 fi
 
+# https://macos-defaults.com/ is a great resource for discovering defaults settings
+
 # Terminal Settings
 # Close the terminal window if the shell exits cleanly
 defaults write com.apple.Terminal shellExitAction 1
 
 # Finder Settings
-defaults write com.apple.finder AppleShowAllFiles YES
-defaults write com.apple.finder ShowPathbar 1
-defaults write com.apple.finder ShowStatusBar 1
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
+defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
+defaults write com.apple.finder "_FXSortFoldersFirst" -bool "true"
+defaults write com.apple.finder "ShowPathbar" -bool "true"
+defaults write com.apple.finder "ShowStatusBar" -bool "true"
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `glyv`
+defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv"
+# Kill all finder instances to reset
+killall Finder
+
+# Input Devices
+
+##### Keyboard
+
+# Mac Globe key opens emoji picker
+defaults write com.apple.HIToolbox AppleFnUsageType -int "2"
 
 # Press and Hold (Extended Characters Popup)
 # Disable Press and Hold in VS Code
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
+# Use tab (shift-tab) to navigate UI Focus
+defaults write NSGlobalDomain AppleKeyboardUIMode -int "2"
+
+# TextEdit
+# Set default format to txt
+defaults write com.apple.TextEdit "RichText" -bool "false"
+killall TextEdit
